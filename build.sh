9895d1c9afae7c44b000a8c3dc670a5922b6f561
bhuwanpandey <bhuwanpandey9867@gmail.com>
2024-08-11 18:19:11 +0545

    updated

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 if [[ $CREATE_SUPERUSER ]];
 then
-  python manage.py createsuperuser --no-input
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
