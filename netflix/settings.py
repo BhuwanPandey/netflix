4741c807155c6ea1e0a0abdc66af66d00a1e6b5f
bhuwanpandey <bhuwanpandey9867@gmail.com>
2024-08-11 17:43:32 +0545

    updated

--- netflix/settings.py
+++ netflix/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 ]
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
     
 
 # Default primary key field type
