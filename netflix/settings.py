8fd2d771b4f2eac7151c13782dc73598e9322d21
bhuwanpandey <bhuwanpandey9867@gmail.com>
2024-08-13 07:49:17 +0545

    reverse db path

--- netflix/settings.py
+++ netflix/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'netflix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db1.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
