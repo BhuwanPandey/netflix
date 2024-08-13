21f3c050916384317ed6845d6707dd1e3b905004
bhuwanpandey <bhuwanpandey9867@gmail.com>
2024-08-13 20:20:58 +0545

    updated image processor

--- users/models.py
+++ users/models.py
@@ -16,13 +16,10 @@ class Profile(models.Model):
         return f'Profile of {self.user.username}'
     
     def save(self,*args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
         if self.photo.path:
             img = Image.open(self.photo.path)
             if img.height > 300 or img.width > 300:
                 output_size = (300, 300)
                 img.thumbnail(output_size)
                 img.save(self.photo.path)
-        super(Profile, self).save(*args, **kwargs)
-        
-
-
